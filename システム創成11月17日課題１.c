801a36bf98c75e61abadfdd8ea1817bd953ead9a
siwasukuma <148538385+siwasukuma@users.noreply.github.com>
2023-11-24 11:27:44 +0900

    Add files via upload

---
+++ システム創成11月17日課題１.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(void)
+
+{
+	char large;//‘å•¶Žš
+	printf("char‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢B");
+	scanf_s("%c", &large);
+if ((large >= 'A') && (large <= 'Z'))
+	{
+		printf("‘å•¶Žš‚ÍŠÜ‚Ü‚ê‚Ä‚¢‚Ü‚·B\n");
+	}
+else 
+	printf("‘å•¶Žš‚ÍŠÜ‚Ü‚ê‚Ä‚¢‚Ü‚¹‚ñB\n");
+	
+	return 0;
+}
+
+
+
+
