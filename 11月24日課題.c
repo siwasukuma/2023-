e867f7da340cd082997e1b40c366dfd75df0c418
siwasukuma <148538385+siwasukuma@users.noreply.github.com>
2023-12-01 14:45:54 +0900

    Add files via upload

---
+++ 11月24日課題.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(void)
+{
+    char str[100];
+    int aaa = 0, bbb = 0, ccc = 0;
+    printf("文字列を入力してください>>>");
+    fgets(str, sizeof(str), stdin);
+    int length = strlen(str);
+
+    //改行文字除去
+    if (length > 0 && str[length-1] == '\n') {
+        str[length-1] = '\0';
+        length--;
+    }
+
+    if (8 <= length && 16 >= length) {
+        for (int i = 0; i < length; i++) {
+            if ('a' <= str[i] && 'z' >=str[i])aaa++;
+            if ('A' <= str[i] && 'Z' >= str[i])bbb++;
+            if (!isalpha((unsigned char) str[i]))ccc++;
+        }
+    }
+    else {
+        printf("8文字以上１６文字以下にしてください\n");
+        return 1;
+    }
+
+    if (aaa >= 1 && bbb >=1 && ccc >= 1)
+        printf("パスワードは設定されました。\n");
+    else {
+        if (aaa == 0)printf("小文字を含んでください\n");
+        if (bbb == 0)printf("大文字を含んでください\n");
+        if (ccc == 0)printf("その他の文字を含んでください\n");
+    }
+
+    return 0;
+}
